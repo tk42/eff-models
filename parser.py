0a27be7e20b32198be385f4a5b4b6f30c78efe1f
tk42 <nsplat@gmail.com>
2025-02-17 11:44:48 +0900

    Update requirements and comment out unused imports in parser.py

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import os
 import io
-import re
+# import re
 import json
 import sys
 import base64
@@ -9,7 +9,7 @@ import openai
 import pdfplumber
 from pydantic import BaseModel
 from typing import Optional
-from google.cloud import vision
+# from google.cloud import vision
 
 
 logger = logging.getLogger(__name__)
@@ -44,26 +44,26 @@ class WoodPrices(BaseModel):
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./serviceaccount.json"
 
 
-def ocr_pdf(pdf_path, dpi=300):
-    client = vision.ImageAnnotatorClient()
+# def ocr_pdf(pdf_path, dpi=300):
+#     client = vision.ImageAnnotatorClient()
 
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            pil_image = page.to_image(resolution=dpi).original
+#     with pdfplumber.open(pdf_path) as pdf:
+#         for page in pdf.pages:
+#             pil_image = page.to_image(resolution=dpi).original
 
-    # PIL の画像を PNG バイト列に変換
-    with io.BytesIO() as buf:
-        pil_image.save(buf, format="PNG")
-        img_bytes = buf.getvalue()
+#     # PIL の画像を PNG バイト列に変換
+#     with io.BytesIO() as buf:
+#         pil_image.save(buf, format="PNG")
+#         img_bytes = buf.getvalue()
 
-    # Cloud Vision へ送信
-    image = vision.Image(content=img_bytes)
-    response = client.document_text_detection(image=image)
-    # texts = response.text_annotations
-    # result = texts[0].description if texts else None
-    texts = response.full_text_annotation
-    result = re.sub(r'(?<!000)\n', '', texts.text)
-    return result
+#     # Cloud Vision へ送信
+#     image = vision.Image(content=img_bytes)
+#     response = client.document_text_detection(image=image)
+#     # texts = response.text_annotations
+#     # result = texts[0].description if texts else None
+#     texts = response.full_text_annotation
+#     result = re.sub(r'(?<!000)\n', '', texts.text)
+#     return result
 
 
 def get_str_from_pdf(pdf_path, dpi=300):
